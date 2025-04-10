942c68db12c5a60ea5c266fb3fd03187df9d4bd4
Jeffery Antoniuk <jeffery.antoniuk@ualberta.ca>
2025-04-09 18:52:02 -0600

    Jupiter  delta: first draft integration between PaperTrail changelog and  Scholaris migration scripts

--- jupiter_output_scripts/jupiter_delta.rb
+++ jupiter_output_scripts/jupiter_delta.rb
@@ -1,6 +1,11 @@
 # Delta a list of changes on Jupiter after a given date
 # base on https://github.com/ualbertalib/jupiter/pull/3626/files
 
+# Parent class from
+# https://gist.github.com/lagoan/839cf8ce997fa17b529d84776b91cdac#file-export_collections_csv-rb-L181-L196
+class ItemCSVExporter < CollectionCSVExporter
+  attr_accessor :easy_dspace_mapping
+end
 
 class ChangesReport
 
@@ -9,9 +14,49 @@ class ChangesReport
     @output_file = output_directory + "#{date}_changes.csv" 
   end
 
+  # Based on and try to reuse the mapping methods in the class
+  # https://gist.github.com/lagoan/839cf8ce997fa17b529d84776b91cdac#file-export_collections_csv-rb-L181-L196
+  def map_change_event_to_scholaris_item(change_event)
+      #result = CollectionCSVExporter.item_data_row(change_event.item)
+      item = change_event.item.decorate
+      ItemCSVExporter.new.easy_dspace_mapping do |method_key, _method_mapping|
+        # keys starting with "manual_" are special cases that need to be handled differently
+        jupiter_key = method_key.sub(/^manual_/,'')
+        # only caputure value if the key exists in the list of object changes in the change event
+        if change_event.object_changes.key?(jupiter_key) do
+          value = if method_key.start_with?('manual_')
+                    handle_manual_value(item, method_key)
+                  else
+                    item.send(method_key)
+                  end
+          if value.is_a?(Array)
+            value.join('||')
+          else
+            value
+          end
+        end
+      end 
+
+  end
+
+  def process_change_event(change_event)
+
+    if change_event.type is Thesis do
+    end
+    else if change_event.item_type is Item do
+      result = map_change_event_to_scholaris_item(change_event)
+    end
+
+    # return dictionary of key value pairs
+    # where the key is the DSpace field name mapped from the Jupiter change event field
+    # and the value is the DSpace field value transformed from the Jupiter change event new field value 
+    return result
+  end
+
+
   def perform()
     CSV.open(@output_file, 'wb') do |csv|
-      csv << ['type', 'id', 'changed at', 'event', 'delta']
+      csv << ['type', 'id', 'changed at', 'event', 'delta', 'scholaris_mapping']
       PaperTrail::Version.where(created_at: @date..).find_each do |row|
         # How to communicate key/value mapping differences from Jupiter to DSpace?
         # First part, add documentation describing how to use the output
@@ -21,6 +66,7 @@ class ChangesReport
         # https://gist.github.com/lagoan/839cf8ce997fa17b529d84776b91cdac#file-export_collections_csv-rb-L181-L196
         # I.e., test if the key exists in the object_changes and creating a new
         # structure in a new column listing the Scholaris key/value pairs to update
+        scholaris_mapping = process_change_event(row)
         csv << [row.item_type, row.item_id, row.created_at, row.event, row.object_changes]
       end
     end
